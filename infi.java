8ad7553ac8e2d3cf6d960392df8c60b2f3057b86
Vansh Saxena <saxenavansh035@gmail.com>
2026-09-14 01:57:48 +0530

    Create infi.java

---
+++ infi.java
@@ -0,0 +1,8 @@
+// Write an infinite loop using do while condition.
+public class pali{
+    public static void main(String[] args) {
+        do{
+            System.out.println("Meowwwww");
+        }while(true);
+    }
+} 
