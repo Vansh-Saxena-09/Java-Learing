da3040d077b42b04add6b195eb82ce6dcba9cf6e
Vansh Saxena <saxenavansh035@gmail.com>
2026-09-22 00:44:24 +0530

    pt4.java

--- pt4.java
+++ pt4.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class pattern4 {
+public class pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
